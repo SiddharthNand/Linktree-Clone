5c581eeafd872fb8ca912d808f9111f7917ef8e3
SiddharthNand <sidharthnand47@gmail.com>
2023-01-28 15:38:06 -0800

    completed need styling

---
+++ app.js
@@ -0,0 +1,18 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+       await navigator.clipboard.writeText(link)
+       alert("Copied to Clipboard: " + link)
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
+shareButtons.forEach(shareButton => 
+    shareButton.addEventListener('click', copyText))
